20ac988e9e2e3c3a0bcb20348ad53d0964c9ba3c
vinibispo <vini.bispo015@gmail.com>
2019-09-05 16:01:20 -0300

    exporting fake controllers

--- controller/User.js
+++ controller/User.js
@@ -13,3 +13,10 @@ function userUpdate() {
 function userDelete() {
     
 }
+
+module.exports = {
+    login:userLogin(),
+    register:userRegister(),
+    update:userUpdate(),
+    delete:userDelete()
+}
