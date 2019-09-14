eeaf6fa8f06394b3f67cbc5244684309e024e9bd
vinibispo <vini.biapo015@gmail.com>
2019-09-14 17:13:44 -0300

    register complete

--- src/index.js
+++ src/index.js
@@ -1,36 +1,12 @@
 const polka = require('polka')
 const bodyParser = require('body-parser')
-const app = polka().listen(3000)
-app.use(bodyParser.json())
 const db = require('./database/config')
-app.get('/', (req, res)=>{
-    db.query("SELECT * FROM Users",(err,row)=>{
-        if(err) res.end(err)
-        res.end(row)
+const control = require('./controller/User')
+const app = polka()
+app.use(bodyParser.json())
+app.get('/users', (request, response)=>{
+    db.query("SELECT * FROM Users", (err, row)=>{
+        if(err) response.end(JSON.stringify(err))
+        response.end(JSON.stringify(row))
     })
 })
-app.post(('/register', (req, res)=>{
-    name = req.body.login
-    pass = req.body.pass
-    control.register(name, pass)
-    res.send(`Enviado com sucesso o nome ${req.body.name}`)}))
-app.post('/login', (req, res)=>{
-    name = req.body.login
-    pass = req.body.pass
-    control.login(name, pass)
-    res.redirect('/')
-})
-app.post('/update', (req, res)=>{
-    name = req.body.login,
-    pass = req.body.pass,
-    id = req.body.id
-    control.update(name, pass, id)
-    res.redirect('/')
-})
-
-app.delete('/delete/:id', (req, res)=>{
-    id = req.params.id
-    control.delete(id)
-    res.send('Usuário deletado')
-})
-module.exports = app
