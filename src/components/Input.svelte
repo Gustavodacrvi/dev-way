1045fb39b96ac56e93e3016ba47ec8601192979b
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-09-23 20:07:58 -0300

    basic login popup and button.svelte

---
+++ src/components/Input.svelte
@@ -0,0 +1,41 @@
+<script>
+  import { createEventDispatcher } from 'svelte'
+
+  const dispatch = createEventDispatcher()
+
+  export let placeholder
+  export let value
+  export let type = "text"
+
+  let str = value
+
+  const emitEvent = () => {
+    dispatch('update', {str})
+  }
+
+</script>
+
+<div class="wrapper">
+  <input class="input" autocomplete="off" placeholder bind:value={str} on:input={emitEvent}>
+</div>
+
+<style>
+
+.wrapper {
+  margin: 14px 0;
+}
+
+.input {
+  outline: none;
+  background: none;
+  border: none;
+  font-family: 'Work Sans';
+  font-size: 1em;
+  width: 100%;
+  padding: 10px;
+  box-sizing: border-box;
+  border-bottom: 1px solid rgb(60,60,60);
+  color: var(--white);
+}
+
+</style>
--- src/components/Popups/Login.svelte
+++ src/components/Popups/Login.svelte
@@ -1,22 +1,63 @@
 <script>
+  import Input from './../Input.svelte'
+  import Button from './../Button.svelte'
 
+  let nomeUsuario = ''
+  let password = ''
+
+  const handleName = ({detail}) => {
+    nomeUsuario = detail.str
+  }
+  const handlePassword = ({detail}) => {
+    password = detail.str
+  }
 </script>
+
+<div class="card">
+  <h1 class="title">Bem Vindo,</h1>
+  <span class="tiny-title">entre na sua conta para continuar</span>
+
+  <div class="inputs">
+    <Input placeholder="nome do usuário:" value={nomeUsuario} on:update={handleName}/>
+    <Input placeholder="senha:" type="password" value={password} on:update={handlePassword}/>
+  </div>
+  <div class="button">
+    <Button value="Continuar"/>
+  </div>
+</div>
 
 <style>
 
+.inputs {
+  margin: 14px 0;
+}
+
+.button {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
 .card {
   flex-basis: 600px;
+  margin: 6px;
   margin-top: 100px;
   background-color: #232222;
   border-radius: 8px;
   z-index: 50;
+  padding: 50px;
+}
+
+.title {
+  color: var(--blue);
+  text-shadow: 0 0 1px rgba(61,111,208,.4);
+  margin: 0;
+}
+
+.tiny-title {
+  color: var(--white);
+  font-size: 1.1em;
 }
 
 </style>
 
-<div class="card">
-  <p>Da onde vc vei mano, aparece do além</p>
-  <p>Da onde vc vei mano, aparece do além</p>
-  <p>Da onde vc vei mano, aparece do além</p>
-  <p>Da onde vc vei mano, aparece do além</p>
-</div>
