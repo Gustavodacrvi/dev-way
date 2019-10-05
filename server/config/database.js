5dc0e0027852a6df8f37427d7c547c8fed29d32f
vinibispo <vini.biapo015@gmail.com>
2019-10-05 10:34:01 -0300

    finally we can use sequelize

--- server/config/database.js
+++ server/config/database.js
@@ -1,3 +1,13 @@
 module.exports = {
-    host: '127.0.0.1'
+    host: '127.0.0.1',
+    username: 'test',
+    password: '123456',
+    database: 'devwaytest',
+    dialect: 'postgres',
+    logging: false,
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true
+    }
 }
--- server/routes/Users.js
+++ server/routes/Users.js
@@ -1,16 +1,5 @@
-const controller = require('../controllers/User')
 const routes = require('express').Router()
-routes.post('/login', (req, res)=>{
-    name = req.body.login
-    pass = req.body.pass
-    control.login(name, pass)
-    res.redirect('/')
-})
-routes.post('/update', (req, res)=>{
-    name = req.body.login,
-    pass = req.body.pass,
-    id = req.body.id
-    control.update(name, pass, id)
-    res.redirect('/')
-})
+const {User} = require('../models')
+
+User.create({name: 'Gustavo', email:'gust@gmail.com', password:'nhggegguhrhurgrug'})
 module.exports = routes
