9d8901a8a5fb146c412459a70d8edbc799512205
vinibispo <vini.biapo015@gmail.com>
2019-10-05 09:36:14 -0300

    using sequelize orm

--- server/config/database.js
+++ server/config/database.js
@@ -1,14 +1,3 @@
-const mysql = require('mysql')
-require('dotenv').config()
-const db =mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DATA,
-  insecureAuth: true
-})
-db.connect((err)=>{
-  if(err) throw err;
-  console.log('Mysql connected')
-})
-module.exports = db
+module.exports = {
+    host: '127.0.0.1'
+}
