c3fc6a8340ba21cebcc96074e889cc7272227075
vinibispo <vini.bispo015@gmail.com>
2019-10-10 14:11:57 -0300

    refactoring second commit

---
+++ server/config/database.js
@@ -0,0 +1,7 @@
+const config = {
+    user: 'test',
+    host: 'localhost',
+    database: 'devwaytest',
+    password: '123456789',
+    port: 3211,
+}
--- server/routes/Users.js
+++ server/routes/Users.js
@@ -1,6 +1,15 @@
+const {Pool} = require('pg')
+const config = require('../config/database')
+const pool = new Pool(config)
 const routes = require('express').Router()
-
-const Controller = require('../controllers/User')
-routes.post('/signup', Controller.signUp)
-routes.post('/login', Controller.login)
+routes.post('/signup', async(req, res)=>{
+    const {name, password, email } = req.body
+    const {rows} = await pool.query(`INSERT INTO Users(name, password, email) VALUES(${name}, ${password}, ${email})`)
+    console.log(rows)
+})
+routes.post('/popup', async (req, res)=>{
+    const {name, password, email} = req.body
+    const {rows} = await pool.query(`SELECT * FROM Users WHERE id=${name} AND password=${password} AND email=${email}`)
+    console.log(rows)
+})
 module.exports = routes
