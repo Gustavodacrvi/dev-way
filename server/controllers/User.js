92b547c3198912ee84e3f6fc7b5bddfe91a3667c
vinibispo <vini.bispo015@gmail.com>
2019-10-13 06:08:52 -0300

    ✨ Bcrypt
    Using bcryptjs

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -1,16 +1,26 @@
 const db = require('../config/database')
 
-function registerUser(name, pass, res){
-    let sql ="INSERT INTO Usuarios(login, senha) VALUES("+name+","+pass+")"
+async function registerUser(name, pass, res){
+    const bcrypt = require('bcryptjs')
+    const hash = await bcrypt.hash(pass, 8)
+    let sql ="INSERT INTO Usuarios(login, senha) VALUES("+name+","+hash+")"
     db.query(sql, (err, rows)=>{
         if (err) console.log(err)
         else res.send(rows)
     })
 }
-function loginUser(name, pass, res){
-    db.query("SELECT * FROM `Usuarios` WHERE `login`=? and `senha`=? ORDER BY `name` asc LIMIT 1", [name, pass], (err, rows)=>{
+async function loginUser(name, pass, res){
+    db.query("SELECT * FROM `Usuarios` WHERE `login`=? ORDER BY `login` asc LIMIT 1", [name], (err, rows)=>{
         if(err) console.log('Erro ao logar')
-        else res.send(rows)
+        else{
+            const user = rows[0]
+            const bcrypt = require('bcryptjs')
+            if (bcrypt.compare(pass, user.senha,()=>{
+                return true
+            }) == true){
+                res.send(user)
+            }
+        }
     })
 }
 module.exports = {
