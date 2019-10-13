67a8ad611b5dccfc28054ada624a0409af0be99d
vinibispo <vini.bispo015@gmail.com>
2019-10-13 05:33:04 -0300

    ♻️ fixing bugs and refactoring

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -1,14 +1,14 @@
 const db = require('../config/database')
 
 function registerUser(name, pass, res){
-    let sql ="INSERT INTO Users(name, password) VALUES("+name+","+password+")"
+    let sql ="INSERT INTO Usuarios(login, senha) VALUES("+name+","+password+")"
     db.query(sql, (err, rows)=>{
         if (err) console.log(err)
         else res.send(rows)
     })
 }
 function loginUser(name, pass, res){
-    db.query("SELECT * FROM `Users` WHERE `name`=? and `password`=? ORDER BY `name` asc LIMIT 1", [name, pass], (err, rows)=>{
+    db.query("SELECT * FROM `Usuarios` WHERE `login`=? and `senha`=? ORDER BY `name` asc LIMIT 1", [name, pass], (err, rows)=>{
         if(err) console.log('Erro ao logar')
         else res.send(rows)
     })
